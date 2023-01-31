5211367c0388b0cf884a979ae822f7b1d917a766
Siddhant Chaudhary <urssidd@gmail.com>
2023-01-31 22:31:24 +0530

    Moving the `preprocess` function to `prepare_functions.py`.

--- assignment1/prepare_functions.py
+++ assignment1/prepare_functions.py
@@ -21,6 +21,28 @@ def save_data(file_path, df):
     """
     df.to_csv(file_path, index=False, header=False)
     
+def preprocess(df, bow_transformer):
+    """
+    Preprocess the data in ``df``. The following preprocessing steps are done.
+
+    1. The labels "ham" and "spam" are converted to 0 and 1 respectively.
+    2. All messages are converted to tokens.
+    3. A count vectorizer is used to convert the tokenized messages to vectors.
+    4. Finally, the vectors are transformed using inverse document frequency and then normalized.
+
+    :param df: Dataframe containing the data to be preprocessed.
+    :param bow_transformer: A ``CountVectorizer`` to transform the sentences in ``df``.
+
+    :returns: The tuple (result_matrix, df["label"])
+    """
+
+    # convert string labels to numeric labels
+    df["label"].replace(["ham", "spam"], [0, 1], inplace=True)
+    sparse_bow = bow_transformer.transform(df["message"])
+    tfidf_transformer = TfidfTransformer().fit(sparse_bow)
+    result_matrix = tfidf_transformer.transform(sparse_bow)
+    return (result_matrix, df["label"])
+
 def prepare_train_validation_test_split(df, train_percent=0.7, test_percent=0.15, seed=None):
     """
     Create a randomized 70-15-15 train/validation/test split.
